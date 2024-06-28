f3f7ac4447882028075ef5a7fff98d77e4d896fa
Teodor Vidakovic <teodorv22@gmail.com>
2024-06-28 19:51:16 +0200

    feat: train.py changes

--- Air Hockey/train.py
+++ Air Hockey/train.py
@@ -6,7 +6,7 @@ from game_core import GameCore
 from gui_core import GUICore
 
 
-def train_maddpg(state_dim, n_actions, load_model=False):
+def train_maddpg(state_dim, n_actions, load_model=False, show_gui_after_episodes=1000, save_interval=100):
     # Initialize the environment
     pygame.init()
     board_image = pygame.image.load("assets/board.png")
@@ -25,63 +25,76 @@ def train_maddpg(state_dim, n_actions, load_model=False):
     steps_done = 0
     clock = pygame.time.Clock()
 
-    for episode in range(n_episodes):
-        print("Episode:" + str(episode))
-        game.reset_game()
-        state_1 = game.get_state(1)
-        state_2 = game.get_state(2)
-        all_states = np.stack([state_1, state_2], axis=0)
-        done = False
+    try:
+        for episode in range(n_episodes):
+            print("Episode:" + str(episode))
+            game.reset_game()
+            state_1 = game.get_state(1)
+            state_2 = game.get_state(2)
+            all_states = np.stack([state_1, state_2], axis=0)
+            done = False
 
-        while not done:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    return
+            while not done:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        agent.save_agents()
+                        pygame.quit()
+                        return
 
-            action = agent.act(all_states)
-            steps_done += 1
-            action1 = (action[0][0].item(), action[0][1].item())
-            action2 = (action[0][2].item(), action[0][3].item())
+                action = agent.act(all_states)
+                steps_done += 1
+                action1 = (action[0][0].item(), action[0][1].item())
+                action2 = (action[0][2].item(), action[0][3].item())
 
-            game.take_action(action1, 1)  # Bot 1 controls paddle 1
-            game.take_action(action2, 2)  # Bot 2 controls paddle 2
+                game.take_action(action1, 1)  # Bot 1 controls paddle 1
+                game.take_action(action2, 2)  # Bot 2 controls paddle 2
 
-            game.update_game_state()
+                game.update_game_state()
 
-            all_next_state = np.stack(
-                [game.get_state(1), game.get_state(2)], axis=0)
+                all_next_state = np.stack(
+                    [game.get_state(1), game.get_state(2)], axis=0)
 
-            reward1 = game.get_reward(1)
-            reward2 = game.get_reward(2)
+                reward1 = game.get_reward(1)
+                reward2 = game.get_reward(2)
 
-            # if reward1 > 0.9 or reward2 > 0.9:
-            #     print("+++++++++++++++++")
-            #     print("Episode:" + str(episode) + " Steps:" + str(steps_done))
-            #     print("Reward1:" + str(reward1))
-            #     print("Reward2:" + str(reward2))
+                # if reward1 > 0.9 or reward2 > 0.9:
+                #     print("+++++++++++++++++")
+                #     print("Episode:" + str(episode) + " Steps:" + str(steps_done))
+                #     print("Reward1:" + str(reward1))
+                #     print("Reward2:" + str(reward2))
 
-            current_time = pygame.time.get_ticks()
-            if current_time - game.last_hit_time > game.max_no_hit_duration:
-                print(f"Skipping episode due to no puck hit in {game.max_no_hit_duration/1000} seconds.")
-                reward1 = -0.6 # Penalty for inactivity
-                reward2 = -0.6  # Penalty for inactivity
-                done = True
-            else:
-                done = not game.running
+                current_time = pygame.time.get_ticks()
+                if current_time - game.last_hit_time > game.max_no_hit_duration:
+                    print(f"Skipping episode due to no puck hit in {game.max_no_hit_duration/1000} seconds.")
+                    reward1 = -0.6 # Penalty for inactivity
+                    reward2 = -0.6  # Penalty for inactivity
+                    done = True
+                else:
+                    done = not game.running
 
-            agent.step(all_states, action, [
-                       reward1, reward2], all_next_state, [done, done])
-            all_states = all_next_state
+                agent.step(all_states, action, [
+                           reward1, reward2], all_next_state, [done, done])
+                all_states = all_next_state
 
-            gui.update(game.paddle1_pos, game.paddle2_pos,
-                       game.puck_pos, game.goals)
-            # game.draw_q_values(screen, target_net1,
-            #                    target_net2, state, game.action_map)
-            pygame.display.flip()
-            clock.tick(500)  # Limit to 240 frames per second
+                if episode >= show_gui_after_episodes:
+                    gui.update(game.paddle1_pos, game.paddle2_pos, game.puck_pos, game.goals)
+                    pygame.display.flip()
 
-            if done:
-                break
+                #gui.update(game.paddle1_pos, game.paddle2_pos,
+                           #game.puck_pos, game.goals)
+                # game.draw_q_values(screen, target_net1,
+                #                    target_net2, state, game.action_map)
+                #pygame.display.flip()
+                clock.tick(500)  # Limit to 240 frames per second
+
+                if done:
+                    break
+
+            if episode % save_interval == 0:
+                agent.save_agents()
+    except KeyboardInterrupt:
+        print("Training interrupted, saving agents...")
+        agent.save_agents()
 
     print("Training completed")
+    pygame.quit()
